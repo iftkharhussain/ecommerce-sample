c4493d0581a63f27c7ba32f0cb723b1f9823709d
iftikhar hussain <iftikhar0750@gmail.com>
2018-12-08 11:35:27 +0500

    build project, move to /docs for github pages

---
+++ docs/precache-manifest.04f4e781f5def928bbe8aa56bcaf8eb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b74d02f59b642dcc5550",
+    "url": "/static/js/main.b74d02f5.chunk.js"
+  },
+  {
+    "revision": "f13555b58cbc54738bb9",
+    "url": "/static/js/1.f13555b5.chunk.js"
+  },
+  {
+    "revision": "61a82ea6fd9fa7d49c7e128e73c85ac2",
+    "url": "/index.html"
+  }
+];
